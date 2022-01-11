ded50d636b93ad6227c98182e24c52bf46931cd9
asachkahitari <patildeep2000@gmail.com>
2022-01-11 17:38:59 +0530

    Amazon done

---
+++ Amazon/Q6.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        return (1 + (m / 2));
+    }
+};
+
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  
